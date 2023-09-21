7b645cbdc169e9fdf5bd813a266d7a96481bd8f7
Maria de Lourdes Carvalho Chaves <95364349+maluchaves91@users.noreply.github.com>
2023-09-21 16:15:32 -0300

    meu primeiro site

---
+++ main.js
@@ -0,0 +1,17 @@
+function voltarTema(){
+    document.getElementById("fundo").style.background="black";
+    document.getElementById("fundo").style.color="white";
+}
+
+function mudarTema(){
+    document.getElementById("fundo").style.background="violet";
+    document.getElementById("fundo").style.color="black ";
+}
+
+function mouseForadaImagem(){
+    document.getElementById("imagem").src="spyro.webp";
+}
+
+function mouseDentrodaImagem(){
+    document.getElementById("imagem").src="cynder.webp";
+}
